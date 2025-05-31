4eea704afb897647817c7214691884b794132eb6
Mateus Severiano <mateusbatatais@gmail.com>
2025-05-31 09:55:07 -0300

    inclui header e footer na home

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,22 +1,28 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/navigation";
-import { ThemeToggle } from "@/components/ThemeToggle/ThemeToggle";
 import { Button } from "@/components/Button/Button";
 import { Home } from "lucide-react";
+import Header from "@/components/Layout/Header/Header";
+import Footer from "@/components/Layout/Footer/Footer";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <Link href="/about">{t("about")}</Link>
-      <ThemeToggle></ThemeToggle>
-      <p className="text-primary ">ala</p>
-      <Button
-        label="teste"
-        size="sm"
-        icon={<Home className="text-secondary " />}
-      />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <div>
+          <h1>{t("title")}</h1>
+          <Link href="/about">{t("about")}</Link>
+          <p className="text-primary ">ala</p>
+          <Button
+            label="teste"
+            size="sm"
+            icon={<Home className="text-secondary " />}
+          />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
