3b915b9ceae0290d58cb3b5c6a59aa011d05fb62
Mateus Severiano <mateusbatatais@gmail.com>
2025-06-02 11:46:24 -0300

    melhoria do layout do auth

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -1,13 +1,38 @@
 // app/[locale]/(auth)/layout.tsx
-import { ReactNode } from "react";
-import "../../globals.scss";
-import { ThemeToggle } from "@/components/ThemeToggle/ThemeToggle";
+"use client";
 
-export default function AuthLayout({ children }: { children: ReactNode }) {
+import { ReactNode } from "react";
+import Image from "next/image";
+import { ThemeToggle } from "@/components/ThemeToggle/ThemeToggle";
+import LanguageSwitcher from "@/components/LanguageSwitcher/LanguageSwitcher";
+import "../../globals.scss";
+import { Link } from "@/i18n/navigation";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function AuthLayout({ children }: Props) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800">
-      <ThemeToggle />
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800">
+      <div className="absolute top-4 right-4 flex items-center space-x-4">
+        <LanguageSwitcher />
+        <ThemeToggle />
+      </div>
+
       <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg dark:bg-gray-900">
+        <div className="flex justify-center mb-6">
+          <Link href="/">
+            <Image
+              src="/images/logo-gamo.svg"
+              alt="Logo Gamo"
+              width={150}
+              height={50}
+              priority={true}
+            />
+          </Link>
+        </div>
+
         {children}
       </div>
     </div>
