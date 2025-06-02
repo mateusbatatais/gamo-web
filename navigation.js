ee3723e9d168010eea4ef620b5bba36e58dec11b
Mateus Severiano <mateusbatatais@gmail.com>
2025-06-02 11:46:08 -0300

    ajusta idiomas

--- navigation.js
+++ navigation.js
@@ -1,6 +1,6 @@
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
-export const locales = ["en", "de", "es", "ja"];
+export const locales = ["en", "pt"];
 export const localePrefix = "as-needed";
 
 export const { Link, redirect, usePathname, useRouter } =
