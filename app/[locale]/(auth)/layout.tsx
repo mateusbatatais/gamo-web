e413356b1c2f4ddcf212c25189ef746fcc7e4294
Mateus Severiano <mateusbatatais@gmail.com>
2025-06-03 14:34:12 -0300

    adiciona limpeza de token

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -7,12 +7,15 @@ import { ThemeToggle } from "@/components/ThemeToggle/ThemeToggle";
 import LanguageSwitcher from "@/components/LanguageSwitcher/LanguageSwitcher";
 import "../../globals.scss";
 import { Link } from "@/i18n/navigation";
+import { useClearInvalidToken } from "@/hooks/useClearInvalidToken";
 
 type Props = {
   children: ReactNode;
 };
 
 export default function AuthLayout({ children }: Props) {
+  useClearInvalidToken();
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800">
       <div className="absolute top-4 right-4 flex items-center space-x-4">
