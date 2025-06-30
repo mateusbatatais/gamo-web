0f2a9ecae07099f350061aacc61d7bc8a4895693
mateusbatatais <mateusbatatais@gmail.com>
2025-06-30 09:15:51 -0300

    remove branch dos parametros da page catalog

--- app/[locale]/(site)/catalog/page.tsx
+++ app/[locale]/(site)/catalog/page.tsx
@@ -7,12 +7,12 @@ import CatalogComponent from "@/components/organisms/Catalog/Catalog";
 
 const CatalogPageWrapper = () => {
   const searchParams = useSearchParams();
-  const brand = searchParams.get("brand") || "";
+  // const brand = searchParams.get("brand") || "";
   const locale = useLocale();
   const page = parseInt(searchParams.get("page") || "1", 10);
   const perPage = parseInt(searchParams.get("perPage") || "6", 10);
 
-  return <CatalogComponent brand={brand} locale={locale} page={page} perPage={perPage} />;
+  return <CatalogComponent locale={locale} page={page} perPage={perPage} />;
 };
 
 export default CatalogPageWrapper;
