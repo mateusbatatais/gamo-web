c9b47798e99931b76e70eeabd4816d819519b092
Mateus Severiano <mateusbatatais@gmail.com>
2025-07-01 14:30:24 -0300

    plugin para analise de strings de tradução faltando

---
+++ i18n-ally.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  baseLocale: "en",
+  locales: ["en", "pt"],
+  localePaths: ["messages"],
+  usage: {
+    scanOnSave: true,
+    checkUnused: true,
+    checkMissing: true,
+  },
+  experimental: {
+    usageMatch: true,
+  },
+};
