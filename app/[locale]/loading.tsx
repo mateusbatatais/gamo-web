e61bb824ee159b4c76c5022b7f7ce17463306faf
mateusbatatais <mateusbatatais@gmail.com>
2025-07-02 09:22:02 -0300

    Melhora loading

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,11 +1,14 @@
 import { Spinner } from "@/components/atoms/Spinner/Spinner";
+import { useTranslations } from "next-intl";
 
 export default function Loading() {
+  const t = useTranslations("");
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/80 dark:bg-gray-900/80">
-      <div className="text-center">
+      <div className="flex flex-col items-center">
         <Spinner variant="primary" size={48} />
-        <p className="mt-4 text-gray-600 dark:text-gray-400">Carregando...</p>
+        <p className="mt-4 text-gray-600 dark:text-gray-400">{t("common.loading")}</p>
       </div>
     </div>
   );
