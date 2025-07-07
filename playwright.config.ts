0e6304e88403662c99237b8d06af130aea9c0876
Mateus Severiano <mateusbatatais@gmail.com>
2025-07-07 12:19:23 -0300

    ultimo test

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,10 +42,6 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "setup",
-      testMatch: /global-setup\.ts/,
-    },
-    {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
