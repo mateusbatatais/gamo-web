55f2f2963e76668679f6d2d05956d89e401c0f19
CO3D <contact@co3d.art>
2025-11-17 16:37:23 -0300

    volta simpleCollapse padrao

--- components/atoms/SimpleCollapse/SimpleCollapse.tsx
+++ components/atoms/SimpleCollapse/SimpleCollapse.tsx
@@ -1,30 +1,29 @@
-// components/atoms/SimpleCollapse/SimpleCollapse.tsx
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
 
 interface SimpleCollapseProps {
-  title: string | React.ReactNode;
+  title: string;
   children: React.ReactNode;
-  isOpen: boolean;
-  onToggle: () => void;
+  defaultOpen?: boolean;
   childrenClass?: string;
 }
 
 export const SimpleCollapse = ({
   title,
   children,
-  isOpen,
-  onToggle,
+  defaultOpen = false,
   childrenClass,
 }: SimpleCollapseProps) => {
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+
   return (
     <div className={`border-b border-gray-200 dark:border-gray-700`}>
       <button
         type="button"
         className="flex cursor-pointer items-center justify-between w-full py-3 text-left font-medium text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 px-2 rounded-md"
-        onClick={onToggle}
+        onClick={() => setIsOpen(!isOpen)}
       >
         <span>{title}</span>
         {isOpen ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
