2b66c03e3b7a737c678cb411701981d4a211dc80
CO3D <contact@co3d.art>
2025-12-03 21:17:50 -0300

    tag manager instalada

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -9,6 +9,7 @@ import { ReactNode } from "react";
 import { Providers } from "@/contexts/Providers";
 import { ThemeProvider } from "next-themes";
 import { DonationModal } from "@/components/organisms/DonationModal/DonationModal";
+import Script from "next/script";
 
 const quicksand = Quicksand({
   subsets: ["latin"],
@@ -96,6 +97,22 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} className={quicksand.className} suppressHydrationWarning>
       <body className="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-55S82573"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-55S82573');`}
+        </Script>
         <ThemeProvider attribute="data-theme" enableSystem>
           <NextIntlClientProvider>
             <Providers>
